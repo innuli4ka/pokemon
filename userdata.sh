54d95fca2a04d1ad758a343228ab360696e851f6
innuli4ka <inna.gots@gmail.com>
2025-07-06 23:12:42 +0300

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -5,11 +5,11 @@ apt update -y
 # Install Git, Python, and pip
 apt install -y git python3 python3-pip
 
-# Clone your repo
-git clone https://github.com/innuli4ka/pokemon.git /home/ubuntu/app
+# Clone your repo as ubuntu user
+sudo -u ubuntu git clone https://github.com/innuli4ka/pokemon.git /home/ubuntu/app
 
 # Install boto3
 pip3 install boto3
 
-# Auto-run ui.py when user logs in
-echo "python3 /home/ubuntu/app/ui.py" >> /home/ubuntu/.bash_profile
+# Append auto-run line to ubuntu's .bash_profile *as ubuntu user*
+sudo -u ubuntu bash -c 'echo "python3 /home/ubuntu/app/ui.py" >> /home/ubuntu/.bash_profile'
