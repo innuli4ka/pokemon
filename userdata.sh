a8e98d52fd333e700e820b27a4a5e1051ee47ea6
innuli4ka <inna.gots@gmail.com>
2025-07-07 19:48:29 +0300

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -2,4 +2,5 @@
     cd /home/ubuntu
     git clone https://github.com/innuli4ka/pokemon.git
     chown -R ubuntu:ubuntu /home/ubuntu/pokemon
-    echo 'if [ -n "$SSH_CONNECTION" ]; then cd ~/pokemon && python3 ui.py; fi' >> /home/ubuntu/.bashrc &
+    echo 'if [ -n "$SSH_CONNECTION" ]; then cd ~/pokemon && python3 ui.py & fi' >> /home/ubuntu/.bashrc
+
