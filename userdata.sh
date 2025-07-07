76a554dd904d9396fc6f5b6767e6bb1e7d9beb70
innuli4ka <inna.gots@gmail.com>
2025-07-07 19:47:40 +0300

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,21 +1,5 @@
-#!/bin/bash
-# Update packages
-yum update -y
-
-# Install Git, Python, pip
-yum install -y git python3
-
-# Make sure pip3 is present
-python3 -m ensurepip
-
-# Upgrade pip
-pip3 install --upgrade pip
-
-# Clone your repo as ec2-user
-sudo -u ec2-user git clone https://github.com/innuli4ka/pokemon.git /home/ec2-user/app
-
-# Install boto3 globally
-pip3 install boto3
-
-# Append auto-run line to ec2-user's .bashrc
-sudo -u ec2-user bash -c 'echo "python3 /home/ec2-user/app/ui.py &" >> /home/ec2-user/.bashrc'
+    #!/bin/bash
+    cd /home/ubuntu
+    git clone https://github.com/innuli4ka/pokemon.git
+    chown -R ubuntu:ubuntu /home/ubuntu/pokemon
+    echo 'if [ -n "$SSH_CONNECTION" ]; then cd ~/pokemon && python3 ui.py; fi' >> /home/ubuntu/.bashrc &
