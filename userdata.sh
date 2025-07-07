2d55bc1d06e62b0b2733102a61d01d0929a28f84
innuli4ka <inna.gots@gmail.com>
2025-07-07 22:49:54 +0300

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,23 @@
-    #!/bin/bash
-    cd /home/ubuntu
-    git clone https://github.com/innuli4ka/pokemon.git
-    chown -R ubuntu:ubuntu /home/ubuntu/pokemon
-    echo 'if [ -n "$SSH_CONNECTION" ]; then cd ~/pokemon && python3 ui.py & fi' >> /home/ubuntu/.bashrc
+#!/bin/bash
 
+# Go to ubuntu's home
+cd /home/ubuntu
+
+# Clone your repo
+git clone https://github.com/innuli4ka/pokemon.git
+chown -R ubuntu:ubuntu /home/ubuntu/pokemon
+
+# Write AWS credentials for boto3
+mkdir -p /home/ubuntu/.aws
+
+cat <<EOF > /home/ubuntu/.aws/credentials
+[default]
+aws_access_key_id = ${aws_access_key}
+aws_secret_access_key = ${aws_secret_key}
+region = ${aws_region}
+EOF
+
+chown -R ubuntu:ubuntu /home/ubuntu/.aws
+
+# Add auto-run to .bashrc
+echo 'if [ -n "$SSH_CONNECTION" ]; then cd ~/pokemon && python3 ui.py; fi' >> /home/ubuntu/.bashrc
